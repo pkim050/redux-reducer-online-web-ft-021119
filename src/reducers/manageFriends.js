f63786740249f7dbef8f0999a95ab23561b0db2f
Patrick Kim <pkim050@gmail.com>
2019-08-01 15:57:20 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      const newFriendList = [...state.friends, action.friend]
+      return {...state, friends: newFriendList}
+    case 'REMOVE_FRIEND':
+      const updatedList = state.friends.filter(friend => friend.id !== action.id)
+      return {...state, friends: updatedList}
+    default:
+      return state
+  }
 }
