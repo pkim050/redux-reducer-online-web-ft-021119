22f9655c53671f8e8df02c9d06fd417502722651
Patrick Kim <pkim050@gmail.com>
2019-09-05 13:31:26 +0000

    Automatically backed up by Learn

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 export function manageFriends(state, action) {
   switch(action.type) {
     case 'ADD_FRIEND':
@@ -6,6 +7,19 @@ export function manageFriends(state, action) {
     case 'REMOVE_FRIEND':
       const updatedList = state.friends.filter(friend => friend.id !== action.id)
       return {...state, friends: updatedList}
+=======
+let action = {
+  type: "ADD_FRIEND",
+  friend: "Chrome Boi"
+}
+
+export function manageFriends(state, action){
+  switch(action.type) {
+    case 'ADD_FRIEND':
+      return state
+    case 'REMOVE_FRIEND':
+      return state
+>>>>>>> 5fd72d77bf3baafa109668f106c4bde126f9044d
     default:
       return state
   }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,7 +1,18 @@
+<<<<<<< HEAD
 export function managePresents(state, action) {
   switch(action.type) {
     case 'INCREASE':
       return {numberOfPresents: state.numberOfPresents + 1}
+=======
+action = {
+  type: "INCREASE"
+}
+
+export function managePresents(state, action){
+  switch(action.type) {
+    CASE 'INCREASE':
+      return {state.numberOfPresents + 1}
+>>>>>>> 5fd72d77bf3baafa109668f106c4bde126f9044d
     default:
       return state
   }
